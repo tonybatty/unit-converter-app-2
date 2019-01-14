f7a74c944156914c42aab54d76a08d572191dd20
Tony Batty <tonyeb87@gmail.com>
2019-01-14 11:16:40 +0000

    add second text input

--- App.js
+++ App.js
@@ -4,19 +4,24 @@ import { StyleSheet, Text, TextInput, View } from "react-native";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: "" };
+    this.state = {
+      unitsFrom: "",
+      unitsTo: ""
+    };
   }
   render() {
     return (
       <View style={styles.container}>
         <TextInput
-          style={{ height: 40, borderWidth: 1 }}
+          style={{ height: 40, borderWidth: 1, width: "100%", padding: 10, margin: 10 }}
           placeholder="Convert From:"
-          onChangeText={text => this.setState({ text })}
           keyboardType="numeric"
-          width="100%"
         />
-        <Text style={{ padding: 10, fontSize: 42 }}>{this.state.text}</Text>
+        <TextInput
+          style={{ height: 40, borderWidth: 1, width: "100%", padding: 10, margin: 10 }}
+          placeholder="Convert To:"
+          keyboardType="numeric"
+        />
       </View>
     );
   }
