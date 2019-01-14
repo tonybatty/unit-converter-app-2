b9faca57dc4e86797b857091361f3e272a8609ef
Tony Batty <tonyeb87@gmail.com>
2019-01-14 11:05:22 +0000

    add text input

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!</Text>
+        <TextInput
+          style={{ height: 40, borderWidth: 1 }}
+          placeholder="Convert From:"
+          onChangeText={text => this.setState({ text })}
+          keyboardType="numeric"
+          width="100%"
+        />
+        <Text style={{ padding: 10, fontSize: 42 }}>{this.state.text}</Text>
       </View>
     );
   }
@@ -14,8 +25,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#f1f1f1",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 15
+  }
 });
