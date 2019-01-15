5de5c268a17f243bc03bb25e27973dcfcdf06fc4
Tony Batty <tonyeb87@gmail.com>
2019-01-15 12:46:08 +0000

    add unit type picker and manually added a few entried to test with

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, Picker, View } from "react-native";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      unitType: "",
+      unitInputFrom: "Meter",
+      unitsInputTo: "Yard",
       unitsFrom: "",
       unitsTo: ""
     };
@@ -12,16 +15,70 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          style={{ height: 40, borderWidth: 1, width: "100%", padding: 10, margin: 10 }}
-          placeholder="Convert From:"
-          keyboardType="numeric"
-        />
-        <TextInput
-          style={{ height: 40, borderWidth: 1, width: "100%", padding: 10, margin: 10 }}
-          placeholder="Convert To:"
-          keyboardType="numeric"
-        />
+        <View style={styles.elementsContainer}>
+        <View style={styles.inputGroup}>
+            <Picker
+              selectedValue={this.state.unitType}
+              style={{ height: 50, flex: 1, borderWidth: 1 }}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({ unitType: itemValue })
+              }
+            >
+              <Picker.Item label="Length" value="Kilometre" />
+              <Picker.Item label="Mass" value="Metre" />
+              <Picker.Item label="Area" value="Centimetre" />
+              <Picker.Item label="Volume" value="Millimetre" />
+            </Picker>
+          </View>
+          <View style={styles.inputGroup}>
+            <TextInput
+              style={{
+                flex: 1,
+                height: 48,
+                borderWidth: 1,
+                padding: 8,
+                textAlign: "right"
+              }}
+              keyboardType="numeric"
+            />
+            <Picker
+              selectedValue={this.state.unitsFrom}
+              style={{ height: 48, flex: 1, borderWidth: 1 }}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({ unitsFrom: itemValue })
+              }
+            >
+              <Picker.Item label="Kilometre" value="Kilometre" />
+              <Picker.Item label="Metre" value="Metre" />
+              <Picker.Item label="Centimetre" value="Centimetre" />
+              <Picker.Item label="Millimetre" value="Millimetre" />
+            </Picker>
+          </View>
+          <View style={styles.inputGroup}>
+            <TextInput
+              style={{
+                flex: 1,
+                height: 48,
+                borderWidth: 1,
+                padding: 8,
+                textAlign: "right"
+              }}
+              keyboardType="numeric"
+            />
+            <Picker
+              selectedValue={this.state.unitsTo}
+              style={{ height: 48, flex: 1, borderWidth: 1 }}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({ unitsTo: itemValue })
+              }
+            >
+              <Picker.Item label="Kilometre" value="Kilometre" />
+              <Picker.Item label="Metre" value="Metre" />
+              <Picker.Item label="Centimetre" value="Centimetre" />
+              <Picker.Item label="Millimetre" value="Millimetre" />
+            </Picker>
+          </View>
+        </View>
       </View>
     );
   }
@@ -30,9 +87,22 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f1f1f1",
-    alignItems: "center",
+    backgroundColor: "#f2f2f2",
+  },
+  elementsContainer: {
+    flex: 1,
+    flexDirection: "column",
     justifyContent: "center",
-    padding: 15
+    marginLeft: 24,
+    marginRight: 24,
+    marginBottom: 24,
+    marginTop: 24
+  },
+  inputGroup: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 12,
+    marginBottom: 12
   }
 });
