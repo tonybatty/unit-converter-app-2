ce712beac1c8d84899ca4955e48ea8b61836e215
Tony Batty <tonyeb87@gmail.com>
2019-01-16 13:51:00 +0000

    selecting unit type changes the unit pickers to convert from and to

--- App.js
+++ App.js
@@ -1,33 +1,78 @@
 import React from "react";
 import { StyleSheet, Text, TextInput, Picker, View } from "react-native";
+import convert from "convert-units";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      unitType: "",
-      unitInputFrom: "Meter",
-      unitsInputTo: "Yard",
-      unitsFrom: "",
-      unitsTo: ""
+      unitType: "length",
+      unitFromSelected: "Meter",
+      unitToSelected: "Yard",
+      unitsFrom: [],
+      unitsTo: []
     };
+
+    this.changeUnitType = this.changeUnitType.bind(this);
   }
+
+  changeUnitType(itemValue, itemIndex) {
+    this.setState({
+      unitType: itemValue,
+      unitsFrom: convert().list(itemValue),
+      unitsTo: convert().list(itemValue)
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.elementsContainer}>
-        <View style={styles.inputGroup}>
+          <View style={styles.inputGroup}>
             <Picker
               selectedValue={this.state.unitType}
               style={{ height: 50, flex: 1, borderWidth: 1 }}
               onValueChange={(itemValue, itemIndex) =>
-                this.setState({ unitType: itemValue })
+                this.changeUnitType(itemValue, itemIndex)
               }
             >
-              <Picker.Item label="Length" value="Kilometre" />
-              <Picker.Item label="Mass" value="Metre" />
-              <Picker.Item label="Area" value="Centimetre" />
-              <Picker.Item label="Volume" value="Millimetre" />
+              {convert()
+                .measures()
+                .map(unitType => (
+                  <Picker.Item
+                    label={unitType}
+                    value={unitType}
+                    key={unitType}
+                  />
+                ))}
+            </Picker>
+          </View>
+          <View style={styles.inputGroup}>
+            <TextInput
+              style={{
+                flex: 1,
+                height: 48,
+                borderWidth: 1,
+                padding: 8,
+                textAlign: "right"
+              }}
+              defaultValue="1"
+              keyboardType="numeric"
+            />
+            <Picker
+              selectedValue={this.state.unitsFromInput}
+              style={{ height: 48, flex: 1, borderWidth: 1 }}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({ unitsFromSelected: itemValue })
+              }
+            >
+              {this.state.unitsFrom.map(unitType => (
+                <Picker.Item
+                  label={unitType.singular}
+                  value={unitType.singular}
+                  key={unitType.singular}
+                />
+              ))}
             </Picker>
           </View>
           <View style={styles.inputGroup}>
@@ -42,40 +87,19 @@ export default class App extends React.Component {
               keyboardType="numeric"
             />
             <Picker
-              selectedValue={this.state.unitsFrom}
+              selectedValue={this.state.unitsToInput}
               style={{ height: 48, flex: 1, borderWidth: 1 }}
               onValueChange={(itemValue, itemIndex) =>
-                this.setState({ unitsFrom: itemValue })
+                this.setState({ unitsToSelected: itemValue })
               }
             >
-              <Picker.Item label="Kilometre" value="Kilometre" />
-              <Picker.Item label="Metre" value="Metre" />
-              <Picker.Item label="Centimetre" value="Centimetre" />
-              <Picker.Item label="Millimetre" value="Millimetre" />
-            </Picker>
-          </View>
-          <View style={styles.inputGroup}>
-            <TextInput
-              style={{
-                flex: 1,
-                height: 48,
-                borderWidth: 1,
-                padding: 8,
-                textAlign: "right"
-              }}
-              keyboardType="numeric"
-            />
-            <Picker
-              selectedValue={this.state.unitsTo}
-              style={{ height: 48, flex: 1, borderWidth: 1 }}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({ unitsTo: itemValue })
-              }
-            >
-              <Picker.Item label="Kilometre" value="Kilometre" />
-              <Picker.Item label="Metre" value="Metre" />
-              <Picker.Item label="Centimetre" value="Centimetre" />
-              <Picker.Item label="Millimetre" value="Millimetre" />
+              {this.state.unitsTo.map(unitType => (
+                <Picker.Item
+                  label={unitType.singular}
+                  value={unitType.singular}
+                  key={unitType.singular}
+                />
+              ))}
             </Picker>
           </View>
         </View>
@@ -87,7 +111,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f2f2f2",
+    backgroundColor: "#f2f2f2"
   },
   elementsContainer: {
     flex: 1,
